3acdda2e70c06a3e5eb77336e9617b615b3ecf52
pyriteC <89157444+pyriteC@users.noreply.github.com>
2022-04-27 13:42:53 -0600

    constructs a 2D array of cells

--- src/fin/controller/Controller.java
+++ src/fin/controller/Controller.java
@@ -7,7 +7,14 @@ public class Controller
 	Cell [][] cells =  new Cell[3][3];
 	public Controller()
 	{
-			
+		for (int index = 0; index < cells.length; index++)
+		{
+			for (int inIndex = 0; inIndex < cells[0].length; inIndex++)
+			{
+				Cell temp = new Cell();
+				cells[index][inIndex] = temp;
+			}
+		}
 	}
 	
 	public static void start()
