6216df3caffe7064b86fb76b4aeb6924a1729e48
pyriteC <89157444+pyriteC@users.noreply.github.com>
2022-05-04 11:30:00 -0600

    java doc for cell

--- src/fin/model/Cell.java
+++ src/fin/model/Cell.java
@@ -7,20 +7,26 @@ public class Cell
 	private int number ;
 	private Color color;
 
-	
+	/**
+	 * basic constructor that sets number to 0 and color to white 
+	 */
 	public Cell()
 	{
 		number = 0;
 		color = new Color(255,255,255);
 		randNum();
 	}
-	
+	/**
+	 * constructor that with ability to set number
+	 * @param number sets the number to whatever is passed
+	 */
 	public Cell( int number)
 	{
 		this.number = number;
 		color = new Color(255,255,255);
 	}
 
+	
 	public int getNumber()
 	{
 		return number;
@@ -41,6 +47,10 @@ public class Cell
 		this.color = color;
 	}
 	
+	/**
+	 * gets the numbers as a string and returns 0 as empty String  
+	 * @return the numbers as a string and returns 0 as empty String  
+	 */
 	public String getNumbAsString()
 	{
 		if (number == 0)
@@ -53,7 +63,9 @@ public class Cell
 			return Integer.toString(number);
 		}
 	}
-	
+	/** 
+	 * 1 in 8 chance of setting the number to 2 if it is currently 0
+	 */
 	public void randNum()
 	{
 		if (number == 0)
